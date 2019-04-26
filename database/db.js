4ef72bfe49bd8618f74b009724c8e5379b118640
usamasshaikh <usama.shaikh1787@gmail.com>
2019-04-26 23:53:10 +0530

    Initial Upload

---
+++ database/db.js
@@ -0,0 +1,19 @@
+const Sequelize = require("sequelize");
+const db = {};
+const sequelize = new Sequelize("nodejs_login", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 3000,
+    idle: 1000
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
